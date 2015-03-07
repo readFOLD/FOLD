c25214cf0e8077c71b666da54807fe54c33beafd
Nathalie H <nhuynh3@mit.edu>
2015-03-07 15:54:28 -0500

    Responding to pull request comments

--- server/accounts.js
+++ server/accounts.js
@@ -7,13 +7,8 @@ Accounts.onCreateUser(function(options, user) {
   if (user.services.twitter) {
     console.log(user.services.twitter);
     user.tempUsername = user.services.twitter.screenName;
-  } else {
-    if(options.profile.name.length<2) {
-      throw new Meteor.Error(403, "Please provide a name.");
-    }
-    if (options.profile) {
-      user.profile = options.profile;
-    }
+  } else if (options.profile){
+    user.profile = options.profile;
   }
   return user;
 });
