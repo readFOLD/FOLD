50e83a8b61e0dd16d807c9559dfea421d5734fbb
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-03-10 18:34:59 -0400

    start saving names

--- server/accounts.js
+++ server/accounts.js
@@ -1,8 +1,51 @@
+
+var reservedUsernames = [
+  'joe',
+  'joegoldbeck',
+  'goldbeck',
+  'joseph',
+  'alexis',
+  'hope',
+  'alexishope',
+  'kevin',
+  'kevinhu',
+  'nathalie',
+  'nhuynh3',
+  'fold',
+  'readfold',
+  'civic',
+  'civicmedia',
+  'whitehouse',
+  'thewhitehouse',
+  'potus',
+  'scotus',
+  'flotus',
+  'medialab',
+  'mit',
+  'thepope',
+  'obama',
+  'barackobama',
+  'pontifex',
+  'usa'
+];
+
+var checkUsername = function(username) {
+  if(username && _.contains(reservedUsernames, username.toLowerCase())){
+    throw new Meteor.Error('This username is reserved. Please email us at fold@media.mit.edu if you have rights to this name.')
+  }
+};
+
+Accounts.validateNewUser(function(user) {
+  checkUsername(user.username);
+  return true
+});
+
 Accounts.onCreateUser(function(options, user) {
  if(!options || !user) {
     throw new Meteor.Error('Error creating user');
   return;
   }
+
   if (options.profile) {
     user.profile = options.profile;
   } else {
