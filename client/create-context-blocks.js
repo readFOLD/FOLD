66a73ee2d1d0aa3425ddd1633f667dafd66f4706
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-03-22 16:03:39 -0400

    _.sample

--- client/create-context-blocks.js
+++ client/create-context-blocks.js
@@ -446,7 +446,7 @@ Template.create_viz_section.onCreated(function() {
   this.countries = VizBlock.countries;
   this.years = [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012];
 
-  this.selectedCountry = new ReactiveVar(this.countries[Math.floor(Math.random() * this.countries.length)].id);
+  this.selectedCountry = new ReactiveVar(_.sample(this.countries).id);
   this.selectedDirection = new ReactiveVar('export');
   this.selectedYear = new ReactiveVar(2012);
 
