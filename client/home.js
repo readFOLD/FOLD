4bee3054d8a77018e60c8133ccefd0a0662e7a69
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-03-22 18:45:51 -0400

    fix twitter signup

--- client/home.js
+++ client/home.js
@@ -28,7 +28,6 @@ loginWithTwitter = function() {
       throw(err); // throw error so we see it on kadira
     } else if (!Meteor.user().username) { // if they are signing up for the first time they won't have a username yet
       Router.go('twitter-signup');
-      Session.set('signingInWithTwitter', false);
     }
     // otherwise they are a returning user, so do nothing because they are now logged in and free to proceed
   });
