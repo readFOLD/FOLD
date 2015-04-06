aee61bdbe6036ba4aae1d99ada32cd0aed266ca5
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-04-06 16:16:13 -0400

    make story favoriting update automatically

--- client/home.js
+++ client/home.js
@@ -123,7 +123,7 @@ Template.filters.events({
 
 Template.all_stories.helpers({
   stories: function() {
-    return Stories.find({ published: true }, {sort: {'publishedAt': 1}, reactive: false}); // TODO update sort based on dropdown selection
+    return Stories.find({ published: true }, {sort: {'publishedAt': 1}}); // TODO update sort based on dropdown selection
   }
 });
 
