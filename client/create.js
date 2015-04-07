00def5aef9f7d564b4d4df3155b39a0845b1c26e
Kevin Hu <kzh@mit.edu>
2015-04-07 14:53:12 -0400

    Fixing context block positioning bug

--- client/create.js
+++ client/create.js
@@ -591,14 +591,8 @@ Template.create_horizontal_section_block.helpers({
 
 Template.create_horizontal_section_block.helpers({
   left: function() {
-    var cardWidth, halfWidth, width;
-    width = Session.get("windowWidth");
-    if (width < 1024) {
-      width = 1024;
-    }
-    halfWidth = width / 2;
-    cardWidth = Session.get("cardWidth");
-    return 75 + halfWidth + (Session.get("separation")) * 1.5;
+    var addBlockWidth = 75;
+    return addBlockWidth + Session.get("verticalLeft") + Session.get("cardWidth") + 2 * Session.get("separation");
   }
 });
 
