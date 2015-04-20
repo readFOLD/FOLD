9cc877f0ddedbbbc54912936c0d98a379ee358bd
Joe Goldbeck <goldbeck.joseph@gmail.com>
2015-04-20 01:30:41 -0400

    fix email check on twitter signup

--- server/accounts.js
+++ server/accounts.js
@@ -7,7 +7,7 @@ checkSignupCode = function(code){
 Accounts.validateNewUser(function(user) {
   if (user.username){ // only if an email user. if twitter user will do this later
     checkSignupCode(user.signupCode);
-    checkUserSignup(user.username, user.emails);
+    checkUserSignup(user.username, user.emails[0].address);
   }
   return true
 });
