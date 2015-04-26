1d24d4d99b51bcdfd455d33e0a2abdc60dbeaa36
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-04-26 00:47:32 -0400

    make profile images retina-ready

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,3 +1,5 @@
 $.cloudinary.config({
   cloud_name: Meteor.settings["public"].CLOUDINARY_CLOUD_NAME
 });
+
+window.isHighDensity = ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 124dpi), only screen and (min-resolution: 1.3dppx), only screen and (min-resolution: 48.8dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (min-device-pixel-ratio: 1.3)').matches)) || (window.devicePixelRatio && window.devicePixelRatio > 1.3));
