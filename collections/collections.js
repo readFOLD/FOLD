183771cde63ee027ba0940ed82849f4b2c8a7dc4
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-04-30 17:25:02 -0400

    fix fold-link creation in firefox

--- collections/collections.js
+++ collections/collections.js
@@ -105,7 +105,7 @@ var cleanHtmlOptions = {
   allowedAttributes: [["data-context-id"],["data-context-type"],["data-context-source"]] // data-context-id is used to direct links to context cards
 };
 
-var matchAnchors =  /<a( data-context-id=["|'].*?["|'])?( data-context-type=["|'].*?["|'])?( data-context-source=["|'].*?["|'])?.*?>/gm; // match anchors, capture data-context-id and other attributes so it can be kept in string
+var matchAnchors =  /<a( data-context-[a-z]*?=["|'].*?["|'])?( data-context-[a-z]*?=["|'].*?["|'])?( data-context-[a-z]*?=["|'].*?["|'])?.*?>/gm; // match anchors, capture data-context-id and other attributes so it can be kept in string
 var matchBlankAnchors = /<a href="javascript:void\(0\);">(.*?)<\/a>/gm; // match anchors that are left over from above if copied from somewhere else, capture contents so can be kept
 
 cleanVerticalSectionContent = function(html) {
