6a67e3e0d8be44bbd0247fbb2997cad0de71926a
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-08-25 17:00:56 -0400

    clean up cleanVerticalSectionContent

--- collections/collections.js
+++ collections/collections.js
@@ -8,41 +8,6 @@ if(!this.Schema){
   Schema = {};
 };
 
-// TO-DO consider replacing htmlclean with https://github.com/cristo-rabani/meteor-universe-html-purifier/
-var cleanHtmlOptions = {
-  allowedTags: ['strong', 'em', 'u', 'b', 'a', 'br'], // only allow tags used in fold-editor and
-  format: false,
-  removeTags: [], // allow u tag
-  removeAttrs: ['class', 'id', 'href'], // strip away hrefs and other undesired attributes that might slip into a paste
-  allowedAttributes: [["data-context-id"],["data-context-type"],["data-context-source"]] // data-context-id is used to direct links to context cards
-};
-
-var matchAnchors =  /<a( data-context-[a-z]*?=["|'].*?["|'])?( data-context-[a-z]*?=["|'].*?["|'])?( data-context-[a-z]*?=["|'].*?["|'])?.*?>/gm; // match anchors, capture data-context-id and other attributes so it can be kept in string
-var matchBlankAnchors = /<a href="javascript:void\(0\);">(.*?)<\/a>/gm; // match anchors that are left over from above if copied from somewhere else, capture contents so can be kept
-
-cleanVerticalSectionContent = function(html) {
-
-  var initialClean = $.htmlClean(html, _.extend({}, _.omit(cleanHtmlOptions, 'allowedTags'), {allowEmpty: ['div']})); // do all cleaning except tag removal. allowEmpty means <div><br></div> turns into <div></div> instead of being deleted entirely
-  
-  var linebreakClean = initialClean
-    .replace(new RegExp('<br />', 'g'), '<br>')
-    .replace(new RegExp('<div><br></div>', 'g'), '<br>')
-    .replace(new RegExp('<div>', 'g'), '<br>')
-    .replace(new RegExp('</div>', 'g'), '');
-
-  return $.htmlClean(linebreakClean, cleanHtmlOptions)
-    .replace(matchAnchors, '<a href="javascript:void(0);"$1$2$3>') // add js void to all anchors and keep all data-context-ids and other data attributes
-    .replace(matchBlankAnchors, '$1') // remove anchors without data-context-ids
-    .replace(new RegExp('<br />', 'g'), '<br>');
-
-};
-
-if (Meteor.isClient) {
-  window.cleanVerticalSectionContent = cleanVerticalSectionContent;
-}
-
-
-
 var analyticsSchema = new SimpleSchema({
   byConnection: {
     type: Number,
