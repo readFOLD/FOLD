7912e5d54ee3c741973eb143dcd6313af3ae2b0e
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-09-08 14:22:54 -0400

    make more clear

--- client/create.js
+++ client/create.js
@@ -125,7 +125,7 @@ window.addContext = function(contextBlock) {
 
 
   contextBlock._id = Random.id(9);
-  contextBlock.rank = null; // places above existing ranked context
+  contextBlock.rank = 0; // places above existing ranked context
 
   Meteor.call('addContextToStream', Session.get("streamShortId"), contextBlock, function(err, contextId){
     saveCallback(err, contextId);
