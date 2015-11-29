a5ab2c94e98d9b65a4b8fceb9bfe6d685a1d9285
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-11-29 14:36:13 -0500

    disallow admin

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ WebApp.connectHandlers.use(function(req, res, next) {
 
 if (_.contains([true, 'true'], process.env.ALLOW_BOTS)){
   robots.addLine('User-agent: *\nDisallow: /create/');
+  robots.addLine('User-agent: *\nDisallow: /admin/');
 } else {
   robots.addLine('User-agent: *\nDisallow: /');
 }
