a0b9145b556aa860693aee54caebf1d99d7bfd81
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-12-04 16:50:47 -0500

    always ask about reload in production

--- client/lib/reload.js
+++ client/lib/reload.js
@@ -1,10 +1,13 @@
 window.readyToMigrate = new ReactiveVar(false);
 
+var reloadDelay = Meteor.settings['public'].NODE_ENV === 'development' ? 0 : 2000;
+
 Reload._onMigrate('fold', function (retry) {
   if (readyToMigrate.get()) {
     return [true, {codeReloaded: true}];
   } else {
-    if (Router.current().route.getName() === 'edit') {
+    //if (Router.current().route.getName() === 'edit') {
+    if (!Meteor.settings['public'].NODE_ENV === 'development') { // true
       notifyDeploy("We've just made an improvement! Click here to sync up the latest code.", true);
       analytics.track('Reload notification happened', {label: 'Reload on click'});
       $('.migration-notification').click(function () {
@@ -25,7 +28,7 @@ Reload._onMigrate('fold', function (retry) {
       setTimeout(function () {
         readyToMigrate.set(true);
         retry();
-      }, 2000);
+      }, reloadDelay);
       return [false]
     }
   }
